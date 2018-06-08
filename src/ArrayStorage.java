9ceaaaa871321b7ab802a2496e7d37aeebcc9f69
ars899 <arsenkadaev@mail.ru>
2018-06-08 12:21:17 +0300

    push

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,26 +6,40 @@ public class ArrayStorage {
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
-        int size = 0;
+        size = 0;
     }
 
     void save(Resume r) {
-
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid)
+                return storage[i];
+            /* Сравнивая запрашиваемое с тем что имеем в массиве и выводим его*/
+        }
+        return null; /* Если нет, то выводить null */
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid)
+                storage[i] = storage[size - 1];
+        }
+        size--;
 
     }
 
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] storage = new Resume[size];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
