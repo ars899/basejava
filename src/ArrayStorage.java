81746755d96e74b7e77ac74a4c6d960244808ab2
ars899 <arsenkadaev@mail.ru>
2018-09-13 12:22:07 +0400

    Clear

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -10,38 +10,23 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        if (size < storage.length) {
-            storage[size] = r;
-            size++;
-        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid)
-                return storage[i];
-
-        }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                return;
-            }
-        }
-
     }
 
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+        return new Resume[0];
     }
 
     int size() {
-        return size;
+        return 0;
     }
 }
