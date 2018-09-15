aa692188599506a9e14f296eb109d8a784c56944
ars899 <arsenkadaev@mail.ru>
2018-09-15 17:09:09 +0400

    Clear

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
-
+/**
+ * Array based storage for Resumes
+ */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size = 0;
@@ -10,20 +12,34 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid)
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid)
+                storage[i] = storage[size - 1];
+
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
